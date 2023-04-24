08835d1c00181e435a57f2d9fda9c82d9deb5ffc
GazizullinRinat <r1natgazizullingazizullin@yandex.ru>
2023-04-24 18:05:09 +0300

    style: удален файл _old

--- _old/add password.py
+++
@@ -1,5 +0,0 @@
-from werkzeug.security import generate_password_hash
-
-
-# для создания пользователей через базу данных
-print(generate_password_hash("1234"))
--- _old/run.py
+++
@@ -1,4 +0,0 @@
-from sweater import app
-
-
-app.run(debug=True, port=4450, host="0.0.0.0")
